6e5513573872421dc1db2f5f7fb4020f8eb758fb
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2020-07-12 12:06:29 -0500

    change port to 28787

--- run_docker.sh
+++ run_docker.sh
@@ -2,7 +2,7 @@
 
 BOOK_DIR=${PWD}/original_book
 
-docker run --rm -p 8787:8787 \
+docker run --rm -p 28787:8787 \
     -v /home/msfz751/docker-share/kurtz:/home/rstudio/share  \
     -e USERID=$UID -e PASSWORD=kurtz \
      -v $BOOK_DIR:/home/rstudio/book \
